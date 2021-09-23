5e31e408fcf27284ae4cfc130f5c1a81b1f36d17
Ben <benjawinther64@gmail.com>
2021-09-22 22:27:59 -0400

    Commit

---
+++ src/Quizes/Quiz14.java
@@ -0,0 +1,13 @@
+package Quizes;
+
+public class Quiz14 {
+    int x = 7;
+    void goBoilers(int x) {
+        System.out.println(this.x);
+    }
+
+    public static void main(String[] args) {
+        Quiz14 q = new Quiz14();
+        q.goBoilers(5);
+    }
+}
